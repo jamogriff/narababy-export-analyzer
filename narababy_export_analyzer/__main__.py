1f4f6632b8be7b47824688dd9dfabccbc2bf379b
Jamison Griffith <jamison.griffith@gmail.com>
2025-09-02 16:09:13 -0600

    Replaces CLI utilities with dedicated library

--- narababy_export_analyzer/__main__.py
+++ narababy_export_analyzer/__main__.py
@@ -1,6 +1,7 @@
 import code
 import sys
 import time
+from bare_cli import BareCLI
 from .narababy_event_log_parser import NarababyEventLogParser
 from .model_factory import ModelFactory
 from .dtos.narababy_bottle_feed_row import NarababyBottleFeedRow
@@ -8,16 +9,17 @@ from .dtos.narababy_diaper_row import NarababyDiaperRow
 from .dtos.narababy_pump_row import NarababyPumpRow
 from .db.db import engine, does_database_exist
 from .models.base import Base
-from .utils.cli_utils import display_info, display_title, display_success, display_failure, get_elapsed_time
+from .utils.cli_utils import get_elapsed_time
 
 if __name__ == "__main__":
+    io = BareCLI()
     parser = NarababyEventLogParser()
-    display_title("Narababy Export Analyzer")
-    file_path = input(f"Enter the file path to the Narababy export CSV: ")
+    io.title("Narababy Export Analyzer")
+    file_path = io.ask(f"Enter the file path to the Narababy export CSV: ")
     try:
         parser.check(file_path)
     except (ValueError, FileNotFoundError) as e:
-        display_failure(str(e))
+        io.error(str(e))
         sys.exit(1)
 
     start = time.perf_counter()
@@ -25,20 +27,20 @@ if __name__ == "__main__":
     end = time.perf_counter()
     capture_percentage = (len(parse_results.data) / parse_results.rows_processed) * 100
     summary = "{0:.0f}% rows captured ({1}/{2}) in {3:.1f} ms".format(capture_percentage, len(parse_results.data), parse_results.rows_processed, get_elapsed_time(start, end))
-    display_success(summary)
+    io.success(summary)
 
     if not does_database_exist(engine):
         Base.create_all()
-        display_success("Created new database.")
+        io.success("Created new database.")
     else:
-        display_info("Database exists.")
+        io.success("Database exists.")
 
     model_factory = ModelFactory(parse_results)
     start = time.perf_counter()
     models = model_factory.make()
     end = time.perf_counter()
     total_models = len(models.babies) + len(models.caregivers) + len(models.bottles) + len(models.diapers) + len(models.pumps)
-    display_success(f"{total_models} models created via DTOs in {get_elapsed_time(start, end):.1f} ms")
+    io.success(f"{total_models} models created via DTOs in {get_elapsed_time(start, end):.1f} ms")
 
 
     local_namespace = dict(globals(), **locals())
