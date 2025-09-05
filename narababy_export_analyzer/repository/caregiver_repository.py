233e83707ae1dec5cb330acbfab76041481e83cb
Jamison Griffith <jamison.griffith@gmail.com>
2025-09-04 19:29:11 -0600

    Adds some caregiver queries

--- narababy_export_analyzer/repository/caregiver_repository.py
+++ narababy_export_analyzer/repository/caregiver_repository.py
@@ -1,7 +1,10 @@
 from sqlalchemy.orm import Session
 from .abstract_repository import AbstractRepository
+from ..models.baby import Baby
 from ..models.caregiver import Caregiver
 from ..models.milk_feed import MilkFeed
+from ..models.diaper_change import DiaperChange
+from ..models.pump import Pump
 
 
 class CaregiverRepository(AbstractRepository):
@@ -9,13 +12,31 @@ class CaregiverRepository(AbstractRepository):
     def __init__(self, session: Session):
         super().__init__(session, Caregiver)
 
-    def get_feeds_by_caregivers(self) -> tuple[str, int]:
+    def get_feeds_by_caregivers(self, baby: Baby) -> list[tuple[str, int]]:
         with self.session as session:
             stmt = (
                 select(self.model.name, func.count(MilkFeed.id))
-                .join(MilkFeed)
+                .outerjoin(MilkFeed)
+                .where(MilkFeed.baby == baby)
                 .group_by(self.model.name)
             )
-            result = self.session.execute(stmt).all()
+            return self.session.execute(stmt).all()
 
-        return result
+    def get_diaper_changes_by_caregivers(self, baby: Baby) -> list[tuple[str, int]]:
+        with self.session as session:
+            stmt = (
+                select(self.model.name, func.count(DiaperChange.id))
+                .outerjoin(DiaperChange)
+                .where(DiaperChange.baby == baby)
+                .group_by(self.model.name)
+            )
+            return self.session.execute(stmt).all()
+
+    def get_pumps_by_caregivers(self) -> list[tuple[str, int]]:
+        with self.session as session:
+            stmt = (
+                select(self.model.name, func.count(Pump.id))
+                .outerjoin(Pump)
+                .group_by(self.model.name)
+            )
+            return self.session.execute(stmt).all()
